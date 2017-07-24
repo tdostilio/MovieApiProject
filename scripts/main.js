93586f5a4e12c9f14ea8acb6459463db03e507e9
tdostilio <tdostilio@gmail.com>
2017-07-24 18:42:42 -0400

    wrote HTML framework for form

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+var OMDb = "http://www.omdbapi.com/";
+var API_KEY = "1ca32dee";
+var POSTER_API = "http://img.omdbapi.com/?i=tt3896198&h=600&apikey=1ca32dee";
+var SEARCH_BUTTON = $('[data-type-submit-button]');
+
+
+
+var Data = {
+    s: "",
+
+}
