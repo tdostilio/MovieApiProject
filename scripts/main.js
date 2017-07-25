27b303210bd9d46c533f3853bcb1095eb4a9bc97
tdostilio <tdostilio@gmail.com>
2017-07-25 14:27:18 -0400

    added expansion functionality for glyphicon

--- scripts/main.js
+++ scripts/main.js
@@ -6,7 +6,7 @@ var $THE_FORM = $('[data-movie-search="form"]');
 var $TITLE = $('[data-type="movieTitle"]');
 // var $GENRE = $('[data-type="genre"]');
 var $SEARCH_RESULTS = $('[data-type="search-results"]');
-var $GLYPHS = $('[data-type="expand"]');
+var GLYPHS = '[data-type="expand"]';
 
 
 $THE_FORM.on('submit', function(event) {
@@ -43,26 +43,50 @@ function presentServerData(obj) {
         }
         var $movieTitle = $('<h4 class="movie-title">'+ key['Title']+'</h4>');
         var $year = $('<p class="movie-year">'+key['Year']+'</p>');
-        var $expand = $('<span class="glyphicon glyphicon-plus expand" data-type="expand" aria-hidden="true"></span>')
-        var titleData = getInfo(key['Title'], "Plot");
-        
+        var $expand = $('<span class="glyphicon glyphicon-plus expand" data-type="expand" aria-hidden="true"></span>');
+        var titleData = getInfo(key['Title']);
         titleData.then( function(data) {
-            var $plot = $('<p class="movie-plot">'+data+'</p>');
+            var $moreContent = $('<div class="more-content hidden"></div>');
+            var $plot = $('<p class="movie-plot">'+data['Plot']+'</p>');
+            var $metascore = $('<p class="metascore">Metascore: '+data['Metascore']+'</p>')
+            $expand.on('click', function(event) {
+                $moreContent.toggleClass("hidden");
+            });
+            if (data['Metascore'] >= 85) {
+                $metascore.addClass('good');
+            } else if (data['Metascore'] >= 70) {
+                $metascore.addClass('ok');
+            } else if (data['Metascore'] < 70) {
+                $metascore.addClass('bad');
+            }
+            $moreContent 
+                .append($plot)
+                .append($metascore)
+            
             $wrapperDiv
                 .append($poster)
                 .append($movieTitle)
                 .append($year)
                 .append($expand)
-                .append($plot)
-        $SEARCH_RESULTS
-            .append($wrapperDiv)
+                .append($moreContent)
+            $SEARCH_RESULTS
+                .append($wrapperDiv)
         })
     }
     )};
 
 
-function getInfo(title, information) {
+function getInfo(title) {
     return $.get((URL+'?t='+title+API_KEY)).then( function(data) {
-        return data[information];
+        return data;
     })
 }
+
+
+// function addClickability() {
+//     $(GLYPHS).on('click', function(event) {
+//     // event.preventDefault();
+//     // $GLYPHS.closest('div').toggleClass('hidden')
+//     console.log("i'm clicking!!!");
+//     });
+// }
