179e7dcf590707acd67d45f2e04b6a5d2ec58645
tdostilio <tdostilio@gmail.com>
2017-07-25 15:55:18 -0400

    fixed rotten tomatoes bug and fixed duplicate title bug

--- scripts/main.js
+++ scripts/main.js
@@ -4,7 +4,6 @@ var POSTER_API = "http://img.omdbapi.com/?i=tt3896198&h=600&apikey=1ca32dee";
 var $SEARCH_BUTTON = $('[data-type-submit-button]');
 var $THE_FORM = $('[data-movie-search="form"]');
 var $TITLE = $('[data-type="movieTitle"]');
-// var $GENRE = $('[data-type="genre"]');
 var $SEARCH_RESULTS = $('[data-type="search-results"]');
 var GLYPHS = '[data-type="expand"]';
 
@@ -44,28 +43,42 @@ function presentServerData(obj) {
         var $movieTitle = $('<h4 class="movie-title">'+ key['Title']+'</h4>');
         var $year = $('<p class="movie-year">'+key['Year']+'</p>');
         var $expand = $('<span class="glyphicon glyphicon-plus expand" data-type="expand" aria-hidden="true"></span>');
-        var titleData = getInfo(key['Title']);
+        var titleData = getInfo(key['Title'],key['Year']);
         titleData.then( function(data) {
             console.log(data);
             var $moreContent = $('<div class="more-content hidden"></div>');
             var $plot = $('<p class="movie-plot">'+data['Plot']+'</p>');
+            if (data['Ratings'][1]) {
             var $rottenTomatoes = $('<p class="rottenTomatoes">Rotten Tomatoes: '+data['Ratings'][1]['Value']+'</p>');
-            var $director = $('<p class="director">'+data['Director']+'</p>');
-            
+            };
+            var $director = $('<p class="director">Director: '+data['Director']+'</p>');
+            var $actors = $('<p class="actors">Actors: '+data['Actors']+'</p>');
+            var $runtime = $('<p class="runtime">Runtime: '+data['Runtime']+'</p>');
+            var $rating = $('<p class="rating">Rated: '+data['Rated']+'</p>');
+            var $boxOffice = $('<p class="box-office">Box Office: '+data['BoxOffice']+'</p>');
+            var $awards = $('<p class="awards">Awards: '+data['Awards']+'</p>');
+
             $expand.on('click', function(event) {
                 $moreContent.toggleClass("hidden");
             });
-            if (parseInt(data['Ratings'][1]['Value']) >= 85) {
-                $rottenTomatoes.addClass('good');
-            } else if (parseInt(data['Ratings'][1]['Value']) >= 65) {
-                $rottenTomatoes.addClass('ok');
-            } else if (parseInt(data['Ratings'][1]['Value']) < 65) {
-                $rottenTomatoes.addClass('bad');
+            if ($rottenTomatoes) {
+                if (parseInt(data['Ratings'][1]['Value']) >= 85) {
+                    $rottenTomatoes.addClass('good');
+                } else if (parseInt(data['Ratings'][1]['Value']) >= 65) {
+                    $rottenTomatoes.addClass('ok');
+                } else if (parseInt(data['Ratings'][1]['Value']) < 65) {
+                    $rottenTomatoes.addClass('bad');
+                }
             }
             $moreContent 
                 .append($plot)
                 .append($rottenTomatoes)
                 .append($director)
+                .append($actors)
+                .append($runtime)
+                .append($rating)
+                .append($boxOffice)
+                .append($awards)
             
             $wrapperDiv
                 .append($poster)
@@ -80,8 +93,8 @@ function presentServerData(obj) {
     )};
 
 
-function getInfo(title) {
-    return $.get((URL+'?t='+title+API_KEY)).then( function(data) {
+function getInfo(title,year) {
+    return $.get((URL+'?t='+title+'&y='+year+API_KEY)).then( function(data) {
         return data;
     })
 }
